f5ab204c0f642480aa75295baf7e43e056d7510f
Lucas Fernandes Andrade <lucasfeandrade@gmail.com>
2017-03-07 01:22:11 -0300

    End of #10 tutorial (Added componentWillMount to todos)

--- 3-flux/src/js/pages/Todos.js
+++ 3-flux/src/js/pages/Todos.js
@@ -11,6 +11,13 @@ export default class Featured extends React.Component {
     };
   }
 
+  componentWillMount() {
+    TodoStore.on("change", () => {
+      this.setState({
+        todos: TodoStore.getAll(),
+      })
+    });
+  }
 
   render() {
     const { todos } = this.state;
